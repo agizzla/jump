d79b6965e727c9b3af7225ade3d49ef05c4ddf6b
agizzla <117372979+agizzla@users.noreply.github.com>
2022-11-03 21:37:45 +0700

    Create get.c

---
+++ get.c
@@ -0,0 +1,7 @@
+public class Example {
+	public static void main(String[] args) {
+		String str = "abcdefgh";
+		char firstChar = str.charAt(0);
+		System.out.println("First Character of given string is : " + firstChar);
+	}
+}
